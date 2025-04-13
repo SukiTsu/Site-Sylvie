7be0fcb997fe21fde87e0d62b0d483d46c186236
SukiTsu <buibrandon47@gmail.com>
2025-04-13 12:29:40 +0200

    correction app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, data } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./page/Home";
 import dataPage from "./page/DescriptionPageData";
 import ContainerPageDescription from "./component/ContainertPageDescription";
